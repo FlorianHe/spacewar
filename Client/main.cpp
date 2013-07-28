a8fa1eabe0fd36b46bd88250febae31a69bbff52
paquet_a <paquet_a@epitech.eu>
2013-07-28 16:29:02 +0200

    Check of display env variable

--- Client/main.cpp
+++ Client/main.cpp
@@ -5,7 +5,7 @@
 // Login   <paquet_a@epitech.net>
 // 
 // Started on  Sun Jul 28 05:37:57 2013 antoine paquet
-// Last update Sun Jul 28 14:51:59 2013 antoine paquet
+// Last update Sun Jul 28 16:27:05 2013 antoine paquet
 //
 
 #include		"SpaceWar.hh"
@@ -14,9 +14,13 @@
 int			main()
 {
   SpaceWar		*game;
+  char			*varDisplay;
 
+  varDisplay = getenv("DISPLAY");
   try
     {
+      if (varDisplay == NULL)
+	throw Except("[ERROR] Environement : Missing display variable");
       game = new SpaceWar();
       game->initialize();
       game->run();
